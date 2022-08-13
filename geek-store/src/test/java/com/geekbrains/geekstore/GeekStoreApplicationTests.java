32c4ea818c71e8b9cf976b3fc40ebe10c35803d0
Roman <rasamaha1905@gmail.com>
2022-08-13 18:49:02 +0300

    Initial commit

---
+++ geek-store/src/test/java/com/geekbrains/geekstore/GeekStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.geekstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
